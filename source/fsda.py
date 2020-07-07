67d42ee60eb2b79385a4a5cf5951af1f6efa2007
adrianbielsa1 <63594921+adrianbielsa1@users.noreply.github.com>
2020-07-07 19:39:03 -0300

    Added function "_compare"

--- source/fsda.py
+++ source/fsda.py
@@ -7,3 +7,22 @@ class State(IntEnum):
     MAYBE   = 1
     ACCEPT  = 2
 
+# Determines whether "actual" string is or can be the "expected" string.
+def _compare(actual: str, expected: str):
+    # Small cache.
+    actual_length   = len(actual)
+    expected_length = len(expected)
+
+    if actual_length < expected_length:
+        if actual == expected[0 : actual_length]:
+            return State.MAYBE
+        else:
+            return State.REJECT
+    elif actual_length == expected_length:
+        if actual == expected:
+            return State.ACCEPT
+        else:
+            return State.REJECT
+    else:
+        return State.REJECT
+
