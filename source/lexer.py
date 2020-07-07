8911add0a38ebc46f2a19cf497588bd015efb8ea
adrianbielsa1 <63594921+adrianbielsa1@users.noreply.github.com>
2020-07-07 19:54:00 -0300

    Removed "Lexer" class

--- source/lexer.py
+++
@@ -1,8 +0,0 @@
-from token      import Token
-from typing     import List
-
-class Lexer:
-
-    # Converts a stream of characters into a stream of tokens.
-    def tokenize(self, text: str) -> List[Token]:
-        pass
